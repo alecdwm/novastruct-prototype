3c82bf73c818ec3021576c814e2097aef3266101
alecdwm <alec@owls.io>
2016-03-18 03:41:42 +0100

    polygon solar system && -/+ zoom

--- prototype/main.lua
+++ prototype/main.lua
@@ -11,6 +11,8 @@ function love.load()
 	player:load()
 	ship:load()
 	player:setParent(ship)
+	ship.x = 280
+	ship.y = -80
 
 	camera:setScale(0.05, 0.05)
 end
@@ -19,8 +21,22 @@ function love.draw()
 	-- World
 	camera:set()
 
-	love.graphics.setColor(17, 27, 26, 255)
+	love.graphics.setColor(241, 217, 26, 255)
 	love.graphics.circle("fill", 0, 0, 50, 32)
+	love.graphics.circle("line", 0, 0, 80, 8)
+
+	love.graphics.setColor(17, 27, 26, 255)
+	love.graphics.circle("fill", 280, -80, 40, 4)
+	love.graphics.circle("line", 280, -80, 60, 4)
+
+	love.graphics.setColor(0, 194, 204, 255)
+	love.graphics.circle("fill", -300, 200, 80, 32)
+	love.graphics.arc("line", -300, 200, 120, 0, 3 * math.pi / 2, 32)
+
+	love.graphics.setColor(240, 80, 20, 255)
+	love.graphics.rectangle("fill", 100, 100, 80, 50)
+	love.graphics.rectangle("line", 90, 90, 100, 70)
+
 	love.graphics.setColor(255, 255, 255, 255)
 	ship:draw()
 	player:draw()
@@ -28,7 +44,7 @@ function love.draw()
 	camera:unset()
 
 	-- UI
-	love.graphics.printf("Controls:\nPlayer: WASD\nShip: Arrow Keys\n\nSwap views with SPACEBAR\nScroll wheel to zoom!", 4, 4, 200, "left")
+	love.graphics.printf("Controls:\nPlayer: WASD\nShip: Arrow Keys\n\nSwap views with SPACEBAR\nScroll wheel to zoom!\n(-/+ also works)", 4, 4, 200, "left")
 	love.graphics.print("Novastruct ap0.001", love.graphics.getWidth(), love.graphics.getHeight(), 0, 1, 1, 140, 18)
 end
 
@@ -58,6 +74,13 @@ function love.keypressed(key, scancode, isrepeat)
 		else
 			camFollow = "ship"
 		end
+	end
+
+	if key == "-" then
+		camera:setScale(camera.sx * 2, camera.sy * 2)
+	end
+	if key == "=" then
+		camera:setScale(camera.sx / 2, camera.sy / 2)
 	end
 end
 
